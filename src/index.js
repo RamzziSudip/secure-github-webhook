b70d2540c4e870ad7befe47b04cb4511a922815d
Sudip Roy <sudiproy20yo@gmail.com>
2022-06-24 01:12:38 +0530

    style: Formatted the intex.js using prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const axios = require("axios");
 const crypto = require("crypto");
-const core = require('@actions/core');
-const hmacSecret = core.getInput('hmacSecret');
+const core = require("@actions/core");
+const hmacSecret = core.getInput("hmacSecret");
 
 if (!hmacSecret || hmacSecret === "" || hmacSecret.trim() === "") {
-  core.setFailed("The hmac secret seems empty. This doesn't seem like what you want.");
+  core.setFailed(
+    "The hmac secret seems empty. This doesn't seem like what you want."
+  );
   return;
 }
 if (hmacSecret.length < 32) {
-  core.setFailed("The hmac secret seems week. You should use at least 32 secure random hex chars.");
+  core.setFailed(
+    "The hmac secret seems week. You should use at least 32 secure random hex chars."
+  );
   return;
 }
 
@@ -30,16 +34,17 @@ function isJsonString(str) {
   }
 }
 
-const url = core.getInput('url');
+const url = core.getInput("url");
 const timeout = parseInt(core.getInput("timeout"));
 const dataInput = core.getInput("data");
 const data = isJsonString(dataInput) ? JSON.parse(dataInput) : dataInput;
 const signature_sha1 = createHmacSignature(data, "sha1");
 const signature_sha256 = createHmacSignature(data, "sha256");
 
-axios.post(url, data, {
-  headers: {
+axios
+  .post(url, data, {
     timeout: timeout,
+    headers: {
       "X-Hub-Signature": signature_sha1,
       "X-Hub-Signature-256": signature_sha256,
       "X-Hub-SHA": process.env.GITHUB_SHA,
