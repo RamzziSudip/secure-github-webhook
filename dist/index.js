e7e2f5f3047f269cc84b4b7a156f7775c9567a36
Sudip Roy <sudiproy20yo@gmail.com>
2022-06-24 01:20:00 +0530

    chore: Update dist

--- dist/index.js
+++ dist/index.js
@@ -2946,23 +2946,27 @@ module.exports.wrap = wrap;
 const axios = __webpack_require__(545);
 const crypto = __webpack_require__(417);
 const core = __webpack_require__(186);
-const hmacSecret = core.getInput('hmacSecret');
+const hmacSecret = core.getInput("hmacSecret");
 
 if (!hmacSecret || hmacSecret === "" || hmacSecret.trim() === "") {
-  core.setFailed("The hmac secret seems empty. This doesn't seem like what you want.");
+  core.setFailed(
+    "The hmac secret seems empty. This doesn't seem like what you want."
+  );
   return;
 }
 if (hmacSecret.length < 32) {
-  core.setFailed("The hmac secret seems week. You should use at least 32 secure random hex chars.");
+  core.setFailed(
+    "The hmac secret seems week. You should use at least 32 secure random hex chars."
+  );
   return;
 }
 
-const createHmacSignature = body => {
-  const hmac = crypto.createHmac("sha256", hmacSecret);
+const createHmacSignature = (body, sha = "sha256") => {
+  const hmac = crypto.createHmac(sha, hmacSecret);
   if (body === "") {
-    return hmac.digest("hex");
+    return sha + "=" + hmac.digest("hex");
   } else {
-    return hmac.update(JSON.stringify(body)).digest("hex");
+    return sha + "=" + hmac.update(JSON.stringify(body)).digest("hex");
   }
 };
 
@@ -2975,21 +2979,34 @@ function isJsonString(str) {
   }
 }
 
-const url = core.getInput('url');
-const dataInput = core.getInput('data');
+const url = core.getInput("url");
+const timeout = parseInt(core.getInput("timeout"));
+const dataInput = core.getInput("data");
 const data = isJsonString(dataInput) ? JSON.parse(dataInput) : dataInput;
-const signature = createHmacSignature(data);
+const signature_sha1 = createHmacSignature(data, "sha1");
+const signature_sha256 = createHmacSignature(data, "sha256");
 
-axios.post(url, data, {
-  headers: {
-    "X-Hub-Signature": signature,
-    "X-Hub-SHA": process.env.GITHUB_SHA
-  }
-}).then(function () {
-  core.info(`Webhook sent sucessfully`)
-}).catch(function (error) {
-  core.setFailed(`Request failed with status code ${error.response.status}`);
-});
+axios
+  .post(url, data, {
+    timeout: timeout,
+    headers: {
+      "X-Hub-Signature": signature_sha1,
+      "X-Hub-Signature-256": signature_sha256,
+      "X-Hub-SHA": process.env.GITHUB_SHA,
+    },
+  })
+  .then(() => {
+    core.info(`Webhook sent sucessfully`);
+  })
+  .catch((error) => {
+    if (error.response == undefined || error.response.status == undefined)
+      core.setFailed(error);
+    else
+      core.setFailed(
+        `Request failed with status code ${error.response.status}`
+      );
+  });
+
 
 /***/ }),
 
